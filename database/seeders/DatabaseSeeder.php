db9612d7afa34b06c7eaa7595941d5f24a90b8d7
david_doichita <daviddoichita@proton.me>
2025-03-27 17:09:37 +0100

    Put seeding in default class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Hash;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $names = ['Euro', 'Bitcoin', 'Ethereum'];
+        $symbols = ['EUR', 'BTC', 'ETH'];
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $len = count($names);
+        for ($i = 0; $i < $len; $i++) {
+            DB::table('cryptos')->insert([
+                'name' => $names[$i],
+                'symbol' => $symbols[$i],
+            ]);
+        }
+
+        DB::table('price_comparison')->insert([
+            'main_id' => '1',
+            'child_id' => '2',
+            'pair_symbol' => 'EUR_BTC',
+            'price' => '0.00001282',
+        ]);
+
+        DB::table('price_comparison')->insert([
+            'main_id' => '1',
+            'child_id' => '3',
+            'pair_symbol' => 'EUR_ETH',
+            'price' => '0.0005',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'surnames' => 'admin',
+            'nif' => '12312323A',
+            'phone_number' => '123123123',
+            'email' => 'admin@local.com',
+            'password' => Hash::make('admin'),
+        ]);
     }
 }
