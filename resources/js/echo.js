88caeb1232ab31d024a72d0d382921e81b838b42
david_doichita <daviddoichita@proton.me>
2025-04-12 12:17:05 +0200

    Hide app secrets

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,20 +1,8 @@
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
-const echo = new Pusher('3651ae947087af488683', {
-    cluster: 'eu',
+const echo = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
 });
-
-// const echo = new Echo({
-//     broadcaster: 'pusher',
-//     // key: import.meta.env.VITE_REVERB_APP_KEY,
-//     key: '3651ae947087af488683',
-//     cluster: 'eu',
-//     // wsHost: import.meta.env.VITE_REVERB_HOST,
-//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
 
 export default echo;
