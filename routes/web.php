5377e20939c9a4d8cfeb0acad406d5e5658e27a5
david_doichita <daviddoichita@proton.me>
2025-04-15 13:53:49 +0200

    Fix sonarqube issue

--- routes/web.php
+++ routes/web.php
@@ -52,5 +52,5 @@ Route::middleware(['auth', 'verified'])->group(function () {
 });
 
 
-require __DIR__ . '/settings.php';
-require __DIR__ . '/auth.php';
+require_once __DIR__ . '/settings.php';
+require_once __DIR__ . '/auth.php';
